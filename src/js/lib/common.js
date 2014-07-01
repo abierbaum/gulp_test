3a6c7a94a6fbfd089f7947e558dc9725534ecd0f
Allen Bierbaum <abierbaum@gmail.com>
2014-07-01 09:48:55 -0500

    Add lodash dep for application through npm.

--- src/js/lib/common.js
+++ src/js/lib/common.js
@@ -2,10 +2,14 @@
 * Common methods here
 */
 /* exported printStuff */
+var _ = require('lodash');
 
 module.exports = {
 
    printStuff: function() {
       console.log('stuff');
+      _.delay(function() {
+         console.log('more stuff');
+      }, 500);
    }
 };
